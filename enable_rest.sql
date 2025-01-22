ab5cd3a7ab1db91ab233f7bbc0778937fa8f2d71
Yuji Nakakoshi <yuji.nkks@gmail.com>
2025-01-22 09:53:59 +0900

    Add files via upload

---
+++ enable_rest.sql
@@ -0,0 +1,20 @@
+set echo on
+
+define WKSPNAME  = &1
+
+--- add roles
+grant connect,resource to WKSP_&WKSPNAME;
+
+-- enable REST service.
+begin
+    ords_admin.enable_schema(
+        p_enabled => true
+        ,p_schema => 'WKSP_&WKSPNAME'
+        ,p_url_mapping_type => 'BASE_PATH'
+        ,p_url_mapping_pattern => '&WKSPNAME'
+        ,p_auto_rest_auth => true
+     );
+     commit;
+end;
+/
+exit;
