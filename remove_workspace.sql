de88e071a0e871698fc1e62a42de411ca650d43d
Yuji Nakakoshi <yuji.nkks@gmail.com>
2025-01-23 14:27:25 +0900

    Update remove_workspace.sql

--- remove_workspace.sql
+++ remove_workspace.sql
@@ -1,10 +1,20 @@
-set echo on
+set echo off
+set serveroutput on
 
 define WKSPNAME  = &1
 
 begin
     apex_instance_admin.remove_workspace('&WKSPNAME');
+exception
+    when others then
+        dbms_output.put_line(SQLERRM);
+end;
+/
+begin
     execute immediate 'drop user wksp_&WKSPNAME cascade';
+exception
+    when others then
+        dbms_output.put_line(SQLERRM);
 end;
 /
 commit;
