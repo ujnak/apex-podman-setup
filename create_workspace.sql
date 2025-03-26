8c92e2f787704adcd0e3938bffa02a6a2595710e
Yuji Nakakoshi <yuji.nkks@gmail.com>
2025-03-26 21:59:43 +0900

    Update create_workspace.sql
    remove connect

--- create_workspace.sql
+++ create_workspace.sql
@@ -13,7 +13,7 @@ begin
     dbms_network_acl_admin.append_host_ace(
         host => '*',
         ace => xs$ace_type(
-            privilege_list => xs$name_list('connect','http'),
+            privilege_list => xs$name_list('http'),
             principal_name => 'wksp_&WKSPNAME',
             principal_type => xs_acl.ptype_db
         )
