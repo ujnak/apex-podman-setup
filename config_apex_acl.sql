9196626d042488fbc8ccda947ef65091aaeb0419
Yuji Nakakoshi <yuji.nkks@gmail.com>
2025-03-27 02:58:08 +0900

    Update config_apex_acl.sql
    change http to connect

--- config_apex_acl.sql
+++ config_apex_acl.sql
@@ -5,7 +5,7 @@ begin
     dbms_network_acl_admin.append_host_ace(
         host => '*',
         ace => xs$ace_type(
-            privilege_list => xs$name_list('http'),
+            privilege_list => xs$name_list('connect'),
             principal_name => '&1',
             principal_type => xs_acl.ptype_db
         )
