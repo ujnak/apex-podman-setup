16840ba353e6c250312662d74aa0d299d171513d
Yuji Nakakoshi <yuji.nkks@gmail.com>
2025-03-27 10:57:57 +0900

    Update config_apex_acl.sql
    add Change History

--- config_apex_acl.sql
+++ config_apex_acl.sql
@@ -1,12 +1,16 @@
 -- --------------------------------------------------------------------
 -- Add Network ACL to APEX schema.
+--
+-- Change History;
+-- 2025-03-27: add http_proxy to privilege_list
+-- 2025-03-27: always upper case for principal_name
 -- --------------------------------------------------------------------
 begin
     dbms_network_acl_admin.append_host_ace(
         host => '*',
         ace => xs$ace_type(
-            privilege_list => xs$name_list('http'),
-            principal_name => '&1',
+            privilege_list => xs$name_list('http','http_proxy'),
+            principal_name => upper('&1'),
             principal_type => xs_acl.ptype_db
         )
     );
