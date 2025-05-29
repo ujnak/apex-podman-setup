f6e296af26926887eb06b8398ecdd5cf1fc87087
Yuji Nakakoshi <yuji.nkks@gmail.com>
2025-05-30 08:55:07 +0900

    Update enable_mle.sql

--- enable_mle.sql
+++ enable_mle.sql
@@ -1,8 +1,6 @@
-#!/bin/sh
-# enable MultiLingual Engine
-# sqlplus sys@localhost/pdb as sysdba
-# usage: @enable_mle <workspace_schema>
-
+-- enable MultiLingual Engine
+-- sqlplus sys@localhost/pdb as sysdba
+-- usage: @enable_mle <workspace_schema>
 grant execute on javascript to &1;
 grant execute dynamic mle to &1;
 grant create mle to &1;
