37915f2376d82321c7e44cc713702d0beed0bcc8
Yuji Nakakoshi <yuji.nkks@gmail.com>
2025-07-03 10:38:11 +0900

    python and r support#

---
+++ config_oml4py.sh
@@ -0,0 +1,116 @@
+#!/bin/sh
+#
+# OML4Py Server and Client configuration script
+# for Oracle Database Free 23ai container.
+#
+# HISTORY
+# 2025/07/03 ynakakos changed to work on the database for APEX.
+# 2025/05/29 ynakakos created.
+#
+# Oracle Machine Learning for Python User's Guide
+# Release 2.1 for Oracle Database 23ai
+# https://docs.oracle.com/en/database/oracle/machine-learning/oml4py/2-23ai/mlpug/
+#
+
+# 4. Install OML4Py for On-Premises Databases
+
+# check OML4Py client 2.1 media 
+if [ ! -f ~/work/oml/V1048628-01.zip ]; then
+    echo "OML4Py client media not found."
+    echo "Download from https://www.oracle.com/database/technologies/oml4py-downloads.html"
+    exit 1
+fi
+
+# 4.2
+# Build and Install Python for Linux for On-Premises Databases
+# https://docs.oracle.com/en/database/oracle/machine-learning/oml4py/2-23ai/mlpug/build-and-install-python-linux-premises-databases.html
+
+# 4.2.1
+# To avoid downloading Python-3.12.6.tgz each time,
+# Python-3.12.6.tgz will be dowonloaded in advance.
+if [ ! -f ~/work/oml/Python-3.12.6.tgz ]; then
+    echo "Python-3.12.6.tgz not found."
+    echo "Download https://www.python.org/ftp/python/3.12.6/Python-3.12.6.tgz"
+    exit 1
+fi
+
+# clear ociregion 
+su -c "echo '' > /etc/dnf/vars/ociregion"
+cat /etc/dnf/vars/ociregion
+
+# 4.2.3
+# Install required os packages + wget and  gcc
+su -c "dnf -y install perl-Env libffi-devel openssl openssl-devel tk-devel xz-devel zlib-devel bzip2-devel readline-devel libuuid-devel ncurses-devel wget gcc"
+
+# 4.2.2
+# Create a directory and extract the contents to this directory
+mkdir -p $HOME/python
+tar -xvzf ~/work/oml/Python-3.12.6.tgz --strip-components=1 -C $HOME/python
+
+# 4.2.4
+# Configure, build and install Python 3.12.6
+cd $HOME/python
+./configure --enable-shared --prefix=$HOME/python
+make clean; make
+make altinstall
+
+# 4.2.5
+# Set environment variable 
+export PYTHONHOME=$HOME/python
+export PATH=$PYTHONHOME/bin:$PATH
+export LD_LIBRARY_PATH=$PYTHONHOME/lib:$LD_LIBRARY_PATH
+
+# 4.2.6
+# Create a symbolic link
+cd $HOME/python/bin
+ln -s python3.12 python3
+
+# Upgrade pip
+cd 
+python3 -m pip install --upgrade pip
+
+# 4.3 Install the Required Supporting Packages for Linux for On-Premises Databases
+# https://docs.oracle.com/en/database/oracle/machine-learning/oml4py/2-23ai/mlpug/install-required-supporting-packages-linux-premises-databases.html
+
+# Installing required packages on OML4Py client machine
+pip3.12 install -r ~/work/requirements.txt
+
+# Installing required packages on OML4Py server machine
+pip3.12 install -r ~/work/requirements2.txt --target=$ORACLE_HOME/oml4py/modules
+
+# 4.4 Install OML4Py Server for On-Premises Oracle Database
+# https://docs.oracle.com/en/database/oracle/machine-learning/oml4py/2-23ai/mlpug/install-oml4py-server-premises-database.html
+
+# 4.4.1
+# Set environment variables
+export ORACLE_HOME=/opt/oracle/product/23ai/dbhomeFree
+export PYTHONHOME=$ORACLE_HOME/python
+export PATH=$PYTHONHOME/bin:$ORACLE_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$PYTHONHOME/lib:$ORACLE_HOME/lib:$LD_LIBRARY_PATH
+
+# Configure OML4Py Server, both on CDB and PDB
+cd $ORACLE_HOME/oml4py/server
+sqlplus / as sysdba <<EOF
+spool install_root.txt
+show con_name
+@pyqcfg.sql SYSAUX TEMP /opt/oracle/product/23ai/dbhomeFree/python
+spool install_pdb.txt
+alter session set container=FREEPDB1;
+show con_name
+@pyqcfg.sql SYSAUX TEMP /opt/oracle/product/23ai/dbhomeFree/python
+exit
+EOF
+
+# 4.5.1.2 Install OML4Py Client for Linux for On-Premises Databases
+# https://docs.oracle.com/en/database/oracle/machine-learning/oml4py/2-23ai/mlpug/install-oml4py-client-linux-premises-databases.html
+
+cd 
+unzip ~/work/oml/V1048628-01.zip
+export PYTHONHOME=$HOME/python
+export PATH=$PYTHONHOME/bin:$PATH
+export LD_LIBRARY_PATH=$PYTHONHOME/lib:$LD_LIBRARY_PATH
+unset PYTHONPATH
+pip3 install client/oml-2.1-cp312-cp312-linux_x86_64.whl
+perl -Iclient client/client.pl -y
+
+# OML4Py configuration complete.
---
+++ config_oml4r.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+#
+# OML4R Server configuration script
+# for Oracle Database Free 23ai container.
+#
+# HISTORY
+# 2025/07/03 ynakakos created.
+#
+# Oracle® Machine Learning for R Installation and Administration Guide
+# Release 2.0 for Oracle Database 23ai
+# https://docs.oracle.com/en/database/oracle/machine-learning/oml4r/2.0.0-23ai/oread/index.html
+#
+
+# 3.2.1 Install Oracle R Distribution on Oracle Linux 8 Using Dnf
+
+# check OML4R supporting packages medium for OML4R 2.0
+if [ ! -f ~/work/oml/oml4r-supporting-linux-x86-64-2.0.zip ]; then
+    echo "OML4R supporting packages medium not found."
+    echo "Download from https://www.oracle.com/database/technologies/oml4r-downloads.html"
+    exit 1
+fi
+
+# clear ociregion 
+su -c "echo '' > /etc/dnf/vars/ociregion"
+cat /etc/dnf/vars/ociregion
+
+# install R-4.0.5
+su -c "yum-config-manager --enable ol8_codeready_builder"
+su -c "yum-config-manager --enable ol8_addons"
+su -c "dnf -y install R-4.0.5 cairo-devel"
+
+# 4.3 Install Oracle Machine Learning for R Server for Oracle Database 23ai
+
+cd $ORACLE_HOME/R/server
+sqlplus / as sysdba <<EOF
+spool install_root.txt
+show con_name
+@rqcfg.sql SYSAUX TEMP /opt/oracle/product/23ai/dbhomeFree /usr/lib64/R
+alter session set container = FREEPDB1;
+spool install_pdb.txt
+show con_name
+@rqcfg.sql SYSAUX TEMP /opt/oracle/product/23ai/dbhomeFree /usr/lib64/R
+exit
+EOF
+
+# 6.4.1 Install the Supporting Packages on Linux
+
+cd ~/work/oml
+if [ ! -d supporting ]; then
+unzip ~/work/oml/oml4r-supporting-linux-x86-64-2.0.zip
+fi
+chmod 755 /opt/oracle/product/23ai/dbhomeFree/bin/ORE
+cd supporting
+ORE CMD INSTALL *
+su -c "R --vanilla CMD INSTALL *"
+
+# OML4R configuration complete.
