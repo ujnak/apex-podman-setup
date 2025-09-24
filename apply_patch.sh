2ad0901b79120bc9898dcaa18115df74d35bb17f
Yuji N <yuji.nkks@gmail.com>
2025-09-24 09:30:45 +0900

    patch 9

--- apply_patch.sh
+++ apply_patch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 podman exec -i apex-db sh <<__EOF__
-cd ~/work/37366599
-cp -rf images ../apex/ 
+cd ~/work/patches/37366599
+cp -rf images ../../apex/ 
 export NLS_LANG=American_America.AL32UTF8
 sqlplus / as sysdba
 alter session set container=freepdb1;
