a72154dc7a6b8317c47512108483a3381c0ff5fc
Yuji N <yuji.nkks@gmail.com>
2025-11-17 11:25:45 +0900

    run tomcat by oracle user

--- rdf4j/setup-oracle-driver.sh
+++ rdf4j/setup-oracle-driver.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-cd $HOME
 # Install Oracle Driver for RDF4J
 cp /opt/oracle/jar/* /opt/tomcat/webapps/rdf4j-server/WEB-INF/lib/
 cp /opt/oracle/jar/* /opt/tomcat/webapps/rdf4j-workbench/WEB-INF/lib/
-cp create-oracle.xsl /opt/tomcat/webapps/rdf4j-workbench/transformations/create-oracle.xsl
-cp create.xsl        /opt/tomcat/webapps/rdf4j-workbench/transformations/create.xsl
+cp /tmp/create-oracle.xsl /opt/tomcat/webapps/rdf4j-workbench/transformations/create-oracle.xsl
+cp /tmp/create.xsl        /opt/tomcat/webapps/rdf4j-workbench/transformations/create.xsl
 # Should update <<username>>, <<pwd>> and <<host:port/servicename>> in context.xml
-cp context.xml       /opt/tomcat/conf/context.xml
+cp /tmp/context.xml       /opt/tomcat/conf/context.xml
 # Should update <must-be-changed> for admin password in tomcat-users.xml
-cp tomcat-users.xml  /opt/tomcat/conf/tomcat-users.xml
+cp /tmp/tomcat-users.xml  /opt/tomcat/conf/tomcat-users.xml
 # Allow access from anywhere
 sed -i 's|127\.0\.0\.0/8,::1/128|0.0.0.0/0,::/0|g' /opt/tomcat/webapps/manager/META-INF/context.xml
 sed -i 's|127\.0\.0\.0/8,::1/128|0.0.0.0/0,::/0|g' /opt/tomcat/webapps/docs/META-INF/context.xml
